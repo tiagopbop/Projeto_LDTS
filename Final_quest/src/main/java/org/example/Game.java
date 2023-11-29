d93f827bb0d8d36d5e72f0a22831946f49fa8392
Tiago Pinheiro <tiagopbop@gmail.com>
2023-11-29 12:00:25 +0000

    Game is new main, added lanterna

---
+++ Final_quest/src/main/java/org/example/Game.java
@@ -0,0 +1,9 @@
+package org.example;
+import java.io.IOException;
+
+public class Game {
+    public  static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
+    }
+}
